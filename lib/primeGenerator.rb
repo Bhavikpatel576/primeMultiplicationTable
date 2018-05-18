2cadc106e80b8e38643c346d980e48996515ea9e
Bhavik Patel <Bhavikpatel576@gmail.com>
2018-05-18 10:18:39 -0500

    create naive function to test for prime values

--- lib/primeGenerator.rb
+++ lib/primeGenerator.rb
@@ -1,4 +1,13 @@
 
 class PrimeGenerator
-
+  def self.isPrime?(number)
+    return false if number < 2
+    return true if number == 2
+    (2...number).each do |value|
+      if (number % value == 0)
+        return false
+      end
+    end
+    true
+  end
 end
--- spec/prime_spec.rb
+++ spec/prime_spec.rb
@@ -1,2 +1,19 @@
 
 require 'rspec'
+require './lib/primeGenerator'
+
+describe PrimeGenerator do 
+
+  it 'determines if a number is prime' do
+    expect(PrimeGenerator.isPrime?(1)).to eql(false)
+    expect(PrimeGenerator.isPrime?(3)).to eql(true)
+    expect(PrimeGenerator.isPrime?(4)).to eql(false)
+    expect(PrimeGenerator.isPrime?(5)).to eql(true)
+    expect(PrimeGenerator.isPrime?(6)).to eql(false)
+    expect(PrimeGenerator.isPrime?(11)).to eql(true)
+  end
+
+  it 'generates prime numbers' do
+    expect(PrimeGenerator.first(10)).to eql([2,3,5,7,11,13,17,19,23,29])
+  end
+end
